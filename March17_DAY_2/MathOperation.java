87c85e119d8c5f3098013e82f942637498cfea24
Edlyn Jessica Philip <edlynjessica09@gmail.com>
2026-03-19 13:21:54 +0530

    Add MathOperation class for basic arithmetic operations

---
+++ March17_DAY_2/MathOperation.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class MathOperation{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("a: ");
+        int a=sc.nextInt();
+        System.out.print("b: ");
+        int b=sc.nextInt();
+        System.out.println("a+b = "+(a+b));
+        System.out.println("a-b = "+(a-b));
+        System.out.println("a*b = "+(a*b));
+        System.out.println("a/b = "+(a/b));
+        System.out.println("a%b = "+(a%b));
+    }
+}
