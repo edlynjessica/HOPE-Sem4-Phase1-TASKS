bbda5a3b87e5f95b4ae0cb5cda94cdd0c57eb1d7
Edlyn Jessica Philip <edlynjessica09@gmail.com>
2026-03-24 14:18:28 +0530

    Add Methods.java with main and square methods

---
+++ March24_DAY_5/CodeBase/Methods.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Methods{
+    public static void main(String[] args) {
+        String s="hello";
+        System.out.println(s);
+        square(2);
+    }
+    public static void square(int n){
+        System.out.println(n*n);
+    }
+}
