fe414902883158e3cc800afed3853147ae5432b3
Edlyn Jessica Philip <edlynjessica09@gmail.com>
2026-03-24 14:19:14 +0530

    Add StringConcept class with main method

---
+++ March24_DAY_5/CodeBase/StringConcept.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class StringConcept{
+    public static void main(String[] args) {
+        String s="hello ";
+        s+="world";
+        System.out.println(s);
+    }
+}
