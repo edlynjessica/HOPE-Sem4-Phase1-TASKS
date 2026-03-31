15f93434d059f1720ded2c7f5dc95ab8fbe5a453
Edlyn Jessica Philip <edlynjessica09@gmail.com>
2026-03-31 21:27:41 +0530

    Create getterSetter.java

---
+++ March31_DAY_10/CodeBase/getterSetter.java
@@ -0,0 +1,16 @@
+class Name{
+    private String name;
+    public void setName(String name){
+        this.name=name;
+    }
+    public String getName(){
+        return this.name;
+    }
+    public class getterSetter{
+        public static void main(String[] args) {
+            Name n=new Name();
+            n.setName("Edlyn");
+            System.out.println("n.getName()");
+        }
+    }
+}
