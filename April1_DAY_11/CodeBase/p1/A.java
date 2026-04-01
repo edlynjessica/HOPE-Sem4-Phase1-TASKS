eab7fc0401289a0f24996db1507526186c0e1d56
Edlyn Jessica Philip <edlynjessica09@gmail.com>
2026-04-01 22:31:34 +0530

    Create A.java

---
+++ April1_DAY_11/CodeBase/p1/A.java
@@ -0,0 +1,20 @@
+public class A{
+    void display(){
+        System.out.println("A");
+    }
+}
+/*
+Packages mean a club of codebase
+
+1. default
+2. custom
+
+default
+-> starts with java
+example: java.util.*
+         java.io.*
+         java.sql.*
+
+custom
+-> starts with com , then application name, then folder structure, codebase
+*/
