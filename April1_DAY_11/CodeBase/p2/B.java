cb015db7443bdca5f53458a1f5fd0bb0aef12bad
Edlyn Jessica Philip <edlynjessica09@gmail.com>
2026-04-01 22:31:54 +0530

    Create B.java

---
+++ April1_DAY_11/CodeBase/p2/B.java
@@ -0,0 +1,14 @@
+package p2;
+import p1.A;
+public class B extends A {
+    public static void main(String[] args) {
+        A obj = new A();
+        obj.display();
+    }
+}
+/*
+
+package -> created by using com as its parent folder
+com/ <package-name> / <module>
+
+*/
