71174cea93fd390bf78c697134a751dbd1486c9f
Edlyn Jessica Philip <edlynjessica09@gmail.com>
2026-04-01 11:19:25 +0530

    Create AutoUnBoxing.java for unboxing example
    Added a Java class to demonstrate auto unboxing.

---
+++ April1_DAY_11/CodeBase/AutoUnBoxing.java
@@ -0,0 +1,9 @@
+public class AutoUnBoxing{
+    public static void main(String[] args){
+        Integer a=127;
+        int obj=a; // a.intValue();
+        System.out.println(obj); 
+
+        // converting the object to primitive datatype is called as unboxing
+    }
+}
