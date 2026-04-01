362e31511ec1b4e9d6d42d38b150ef70f5e605d2
Edlyn Jessica Philip <edlynjessica09@gmail.com>
2026-04-01 19:53:51 +0530

    Create Product.java

---
+++ April1_DAY_11/CodeBase/MiniProject/com/ecommerce/model/Product.java
@@ -0,0 +1,17 @@
+package com.ecommerce.model;
+public class Product{
+    private int id;
+    private String name;
+    private int price;
+    public Product(int id, String name, int price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+    public int getPrice() {
+        return this.price;
+    }
+    public void display() {
+        System.out.println(this.id + " " + this.name + " Rs." + this.price);
+    }
+}
