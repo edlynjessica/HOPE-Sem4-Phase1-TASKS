0c5609c4bb158aeed0a60118ea03946d690e04ff
Edlyn Jessica Philip <edlynjessica09@gmail.com>
2026-04-17 22:18:16 +0530

    Create DefaultInterfaceImp2.java

---
+++ April17_DAY_20/DefaultInterfaceImp2.java
@@ -0,0 +1,19 @@
+ interface Method{
+    default void display(){
+        System.out.println("welcome to interface implementation");
+    }
+}
+class Hello implements Method{
+/*
+     public void display(){
+         System.out.println("overrided the old");
+     }
+*/
+}
+
+public class DefaultInterfaceImp2{
+    public static void main(String[] args) {
+        Hello h=new Hello();
+        h.display();
+    }
+}
